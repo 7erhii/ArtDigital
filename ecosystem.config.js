bfac567acbfe4b32d95ee2fe4ea42b775d3b1380
7erhiy <7erhiy@gmail.com>
2024-07-31 15:52:05 -0300

    check push

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,19 @@
 module.exports = {
-    apps: [{
-      name: 'ArtDigital',
-      script: 'npm',
-      args: 'start',
-      watch: true,
-      env: {
-        NODE_ENV: 'production',
-      }
-    }]
-  };
-  
+  apps : [{
+    name: 'ArtDigital',
+    script: 'npm',
+    args: 'start',
+    watch: true,
+    env: {
+      NODE_ENV: 'production',
+    }
+  },
+  {
+    name: 'gitscript',
+    script: '/home/artupdate/server.js',
+    watch: true,
+    env: {
+      NODE_ENV: 'production',
+    }
+  }]
+};
