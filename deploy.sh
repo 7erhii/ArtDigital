2ee369ad87bf2a34eea350a0747616ce16ffdd6a
7erhiy <7erhiy@gmail.com>
2024-07-31 19:11:44 -0300

    check push

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+
 PROJECT_DIR="/home/ArtDigital"
 LOG_FILE="/home/ArtDigital/deploy.log"
 
 cd $PROJECT_DIR || exit
 
+git stash save "Auto stash before pull" >> $LOG_FILE 2>&1
+git stash drop stash@{1} >> $LOG_FILE 2>&1
+
 git pull origin main >> $LOG_FILE 2>&1
+
 
 npm i >> $LOG_FILE 2>&1
 
