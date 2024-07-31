e54f410d75aa2cd61eaeca5571bdcf423b429cc9
7erhiy <7erhiy@gmail.com>
2024-07-31 19:04:37 -0300

    check push

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
 PROJECT_DIR="/home/ArtDigital"
+LOG_FILE="/home/ArtDigital/deploy.log"
 
 cd $PROJECT_DIR || exit
 
-git pull origin main
+git pull origin main >> $LOG_FILE 2>&1
 
-npm i
+npm i >> $LOG_FILE 2>&1
 
-npm run build
+npm run build >> $LOG_FILE 2>&1
 
-pm2 restart ArtDigital
+pm2 restart ArtDigital >> $LOG_FILE 2>&1
