1c5352d32be11689d06c34fda45c4368288977bb
7erhiy <7erhiy@gmail.com>
2024-07-31 18:46:17 -0300

    check push

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-# Путь к проекту
 PROJECT_DIR="/home/ArtDigital"
 
-# Переход в директорию проекта
-cd $PROJECT_DIR
+cd $PROJECT_DIR || exit
 
-# Обновление репозитория
 git pull origin main
 
-# Установка зависимостей
-npm install
+npm i
 
-# Сборка проекта
 npm run build
 
-# Перезапуск приложения с использованием PM2
 pm2 restart ArtDigital
