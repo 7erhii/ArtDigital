69a069e2d5648846fa99b4fa3afed4b7f2d00236
7erhiy <7erhiy@gmail.com>
2024-07-31 19:50:08 -0300

    check push

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,30 @@
 PROJECT_DIR="/home/ArtDigital"
 LOG_FILE="/home/ArtDigital/deploy.log"
 
-cd $PROJECT_DIR || exit
+log() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> $LOG_FILE
+}
 
+cd $PROJECT_DIR || { log "Ошибка: Не удалось перейти в директорию проекта"; exit 1; }
+
+log "Начало развертывания"
+
+log "Добавление изменений в git"
 git add . >> $LOG_FILE 2>&1
+
+log "Создание коммита в git"
 git commit -m "Auto commit before pull" >> $LOG_FILE 2>&1
 
+log "Обновление из удаленного репозитория"
 git pull origin main >> $LOG_FILE 2>&1
 
+log "Установка  npm"
 npm i >> $LOG_FILE 2>&1
+
+log "Сборка "
 npm run build >> $LOG_FILE 2>&1
 
+log "Перезапуск  PM2"
 pm2 restart ArtDigital >> $LOG_FILE 2>&1
+
+log "завершено"
