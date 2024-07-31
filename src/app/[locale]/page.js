070fc7e39b52ceec9d001e620b013f97785f1d45
7erhiy <7erhiy@gmail.com>
2024-07-31 18:39:29 -0300

    check push

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -65,11 +65,9 @@ export default function HomePage({ params: { locale } }) {
     },
   };
 
-
-  
   return (
     <div>
-      <Header locale={locale} />
+      {/* <Header locale={locale} />
       <Hero />
       <AchievementsSection />
       <DesignSection />
@@ -83,7 +81,7 @@ export default function HomePage({ params: { locale } }) {
       </SectionWrapper>
       <ContactForm data={contactFormData} />
       <Faq page="MainPageItems" />
-      <Footer locale={locale} />
+      <Footer locale={locale} /> */}
       <Colophon />
     </div>
   );
