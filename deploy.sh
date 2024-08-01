b411802c71a8b3514b7913157af2c481a331515a
7erhiy <7erhiy@gmail.com>
2024-07-31 23:33:51 -0300

    check push to sv

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,24 @@
 
 LOG_FILE="/home/ArtDigital/deploy.log"
 PROJECT_DIR="/home/ArtDigital"
+GIT_BRANCH="main" # Укажите нужную ветку
 
 echo "Starting deployment at $(date)" >> $LOG_FILE
 
 # Переход в директорию проекта
 cd $PROJECT_DIR || { echo "Failed to change directory to $PROJECT_DIR" >> $LOG_FILE; exit 1; }
 
-# Обновление из репозитория
-git pull --ff-only >> $LOG_FILE 2>&1
+# Получение последней версии из репозитория
+git fetch origin >> $LOG_FILE 2>&1
 if [ $? -ne 0 ]; then
-    echo "Git pull failed" >> $LOG_FILE
+    echo "Git fetch failed" >> $LOG_FILE
+    exit 1
+fi
+
+# Сброс к последнему коммиту удаленной ветки
+git reset --hard origin/$GIT_BRANCH >> $LOG_FILE 2>&1
+if [ $? -ne 0 ]; then
+    echo "Git reset failed" >> $LOG_FILE
     exit 1
 fi
 
