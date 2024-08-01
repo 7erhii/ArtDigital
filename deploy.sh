ce0067f008130c744e83171f0ccefd5135db35f9
7erhiy <7erhiy@gmail.com>
2024-07-31 23:46:17 -0300

    check push to sv

--- deploy.sh
+++ deploy.sh
@@ -1,42 +1,22 @@
 #!/bin/bash
 
-LOG_FILE="/home/ArtDigital/deploy.log"
 PROJECT_DIR="/home/ArtDigital"
-GIT_BRANCH="main" 
+GIT_BRANCH="main"
 
-echo "Starting deployment at $(date)" >> $LOG_FILE
+echo "Starting deployment at $(date)"
 
-cd $PROJECT_DIR || { echo "Failed to change directory to $PROJECT_DIR" >> $LOG_FILE; exit 1; }
+cd $PROJECT_DIR || { echo "Failed to change directory to $PROJECT_DIR"; exit 1; }
 
-git fetch origin >> $LOG_FILE 2>&1
+git fetch origin && git reset --hard origin/$GIT_BRANCH
 if [ $? -ne 0 ]; then
-    echo "Git fetch failed" >> $LOG_FILE
+    echo "Git update failed"
     exit 1
 fi
 
-git reset --hard origin/$GIT_BRANCH >> $LOG_FILE 2>&1
-if [ $? -ne 0 ]; then
-    echo "Git reset failed" >> $LOG_FILE
-    exit 1
-fi
+npm install || { echo "NPM install failed"; exit 1; }
 
-npm install >> $LOG_FILE 2>&1
-if [ $? -ne 0 ]; then
-    echo "NPM install failed" >> $LOG_FILE
-    exit 1
-fi
+npm run build || { echo "NPM build failed"; exit 1; }
 
-npm run build >> $LOG_FILE 2>&1
-if [ $? -ne 0 ]; then
-    echo "NPM build failed" >> $LOG_FILE
-    exit 1
-fi
+pm2 restart ArtDigital || { echo "PM2 restart failed"; exit 1; }
 
-pm2 restart ArtDigital >> $LOG_FILE 2>&1
-if [ $? -ne 0 ]; then
-    echo "PM2 restart failed" >> $LOG_FILE
-    exit 1
-fi
-
-echo "Deployment completed at $(date)" >> $LOG_FILE
-
+echo "Deployment completed at $(date)"
